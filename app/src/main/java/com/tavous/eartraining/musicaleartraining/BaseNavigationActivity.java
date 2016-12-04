f6fac763bd3cc468fd20ec270cdb23854f776791
A.Tavoosi <tavousi.ali@gmail.com>
2016-12-04 16:37:44 +0330

    T

---
+++ app/src/main/java/com/tavous/eartraining/musicaleartraining/BaseNavigationActivity.java
@@ -0,0 +1,149 @@
+package com.tavous.eartraining.musicaleartraining;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Callable;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public abstract class BaseNavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    private DrawerLayout drawer;
+    boolean doubleBackToExitPressedOnce = false;
+
+    protected void setContent(int layout) {
+        setContentView(layout);
+    }
+
+    protected void onCreateBase(boolean hasDrawer) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (!hasDrawer)
+            drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+
+        try {
+            String label = getResources().getString(getPackageManager().getActivityInfo(getComponentName(), 0).labelRes);
+            TextView textView = (TextView) toolbar.findViewById(R.id.activity_title);
+            textView.setText(label);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void onBackClicked(Callable func, String className) {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            if (className == "MainActivity") {
+                if (doubleBackToExitPressedOnce) {
+                    try {
+                        func.call();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    super.onBackPressed();
+                    return;
+                }
+
+                this.doubleBackToExitPressedOnce = true;
+                Toast.makeText(this, "برای خروج بار دیگر کلیک کنید", Toast.LENGTH_SHORT).show();
+
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        doubleBackToExitPressedOnce = false;
+                    }
+                }, 2000);
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+    }
+
+    public void onNavigationItemSelected(MenuItem item, Context context) {
+        drawer.closeDrawer(GravityCompat.END);
+
+        if ((context.getClass().getName().contains("MainActivity") && item.getItemId() == R.id.mainPage) ||
+                (context.getClass().getName().contains("SimilarAppActivity") && item.getItemId() == R.id.similarapp) ||
+                (context.getClass().getName().contains("ContactusActivity") && item.getItemId() == R.id.contactus) ||
+                (context.getClass().getName().contains("AboutActivity") && item.getItemId() == R.id.about))
+            return;
+
+        int id = item.getItemId();
+
+        if (id == R.id.mainPage) {
+            try {
+                startActivity(new Intent(context, MainActivity.class));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+//        else if (id == R.id.about) {
+//            try {
+//                startActivity(new Intent(context, AboutActivity.class));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        } else if (id == R.id.contactus) {
+//            try {
+//                startActivity(new Intent(context, ContactusActivity.class));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        } else if (id == R.id.similarapp) {
+//            try {
+//                startActivity(new Intent(context, SimilarAppActivity.class));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+
+    public void onCreateOptionsMenuBase(Menu menu, boolean isBackButton) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        if (isBackButton)
+            menu.getItem(0).setIcon(getResources().getDrawable(R.drawable.ic_menu_back));
+    }
+
+    public boolean onOptionsItemSelectedBase(MenuItem item, boolean isBackButton) {
+        int id = item.getItemId();
+
+        if (id == R.id.navigetion_icon) {
+            if (isBackButton) {
+                finish();
+            } else {
+                drawer.openDrawer(GravityCompat.END);
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/tavous/eartraining/musicaleartraining/MainActivity.java
+++ app/src/main/java/com/tavous/eartraining/musicaleartraining/MainActivity.java
@@ -1,300 +1,60 @@
 package com.tavous.eartraining.musicaleartraining;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.gson.Gson;
+import java.util.concurrent.Callable;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
+public class MainActivity extends BaseNavigationActivity {
 
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
-
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    //Task
-    private MediaPlayer mp;
-    TextView tvNoteName;
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
-
-    private DrawerLayout drawer;
+    Button session1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.setContent(R.layout.activity_main);
+        super.onCreateBase(true);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        tvNoteName = (TextView) findViewById(R.id.NoteName);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-
-        Thread thread = new Thread(new ExecuteSessionRunnable());
-        thread.start();
-
-    }
-
-    ;
-
-    public class ExecuteSessionRunnable implements Runnable {
-
-        @Override
-        public void run() {
-
-            InputStream session = getResources().openRawResource(R.raw.session1);
-
-            try {
-                String sessionContent = inputStreamToString(session);
-
-                Gson gson = new Gson();
-                Session s = gson.fromJson(sessionContent, Session.class);
-
-                for (final Session.Note note : s.Note) {
-                    try {
-
-                        tvNoteName.post(new Runnable() {
-                            public void run() {
-                                Animation anim_fade_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
-                                if (note.Show)
-                                    tvNoteName.setText(GetNoteText(note.Name));
-                                else
-                                    tvNoteName.setText("");
-                                tvNoteName.setBackgroundColor(Color.parseColor(GetColorCode(note.Name)));
-                                tvNoteName.setTextColor(Color.parseColor("#ffffff"));
-                                tvNoteName.startAnimation(anim_fade_in);
-
-                                if (mp != null) {
-                                    mp.release();
-                                    mp = null;
-                                }
-                                mp = MediaPlayer.create(MainActivity.this, GetNoteSoundId(note.Name));
-                                mp.start();
-                            }
-                        });
-                        Log.d("Ali", String.valueOf(note.Duration) + "___" + note.Name);
-                        Thread.sleep(note.Duration);
-                    } catch (InterruptedException ex) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
-
-            } catch (Exception e) {
+        session1 = (Button) findViewById(R.id.session1);
+        session1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SessionActivity.class);
+                startActivity(intent);
             }
-        }
-
-    }
-
-    private String GetColorCode(String noteName) {
-        String ColorCode;
-        switch (noteName) {
-            case "A":
-                ColorCode = "#3C00FF";
-                break;
-            case "B":
-                ColorCode = "#FF00FD";
-                break;
-            case "C":
-            case "C2":
-                ColorCode = "#FF0000";
-                break;
-            case "D":
-                ColorCode = "#DB7B00";
-                break;
-            case "E":
-                ColorCode = "#E4ED00";
-                break;
-            case "F":
-                ColorCode = "#81D700";
-                break;
-            case "G":
-                ColorCode = "#00FFEA";
-                break;
-            default:
-                ColorCode = "#000000";
-        }
-
-        return ColorCode;
-    }
-
-    private int GetNoteSoundId(String noteName) {
-        int SoundId;
-        switch (noteName) {
-            case "A":
-                SoundId = R.raw.a;
-                break;
-            case "B":
-                SoundId = R.raw.b;
-                break;
-            case "C":
-                SoundId = R.raw.c;
-                break;
-            case "D":
-                SoundId = R.raw.d;
-                break;
-            case "E":
-                SoundId = R.raw.e;
-                break;
-            case "F":
-                SoundId = R.raw.f;
-                break;
-            case "G":
-                SoundId = R.raw.g;
-                break;
-            case "C2":
-                SoundId = R.raw.c2;
-                break;
-            default:
-                SoundId = R.raw.a;
-        }
-
-        return SoundId;
-    }
-
-    private String GetNoteText(String noteName) {
-        String NoteText;
-        switch (noteName) {
-            case "A":
-            case "a":
-                NoteText = "A";
-                break;
-            case "B":
-            case "b":
-                NoteText ="B";
-                break;
-            case "C":
-            case "c":
-            case "C2":
-            case "c2":
-                NoteText = "C";
-                break;
-            case "D":
-            case "d":
-                NoteText = "D";
-                break;
-            case "E":
-            case "e":
-                NoteText = "E";
-                break;
-            case "F":
-            case "f":
-                NoteText = "F";
-                break;
-            case "G":
-            case "g":
-                NoteText = "G";
-                break;
-            default:
-                NoteText = "A";
-        }
-
-        return NoteText;
-    }
-    private String inputStreamToString(InputStream session) throws IOException {
-        Writer writer = new StringWriter();
-        char[] buffer = new char[1024];
-        try {
-            Reader reader = new BufferedReader(new InputStreamReader(session, "UTF-8"));
-            int n;
-            while ((n = reader.read(buffer)) != -1) {
-                writer.write(buffer, 0, n);
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        return writer.toString();
+        });
     }
 
     @Override
     public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.END)) {
-            drawer.closeDrawer(GravityCompat.END);
-        } else {
-            super.onBackPressed();
-        }
+        super.onBackClicked(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                return null;
+            }
+        }, "MainActivity");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        super.onCreateOptionsMenuBase(menu, false);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.navigetion_icon) {
-            drawer.openDrawer(GravityCompat.END);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelectedBase(item, false);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        drawer.closeDrawer(GravityCompat.END);
+        super.onNavigationItemSelected(item, this);
         return true;
     }
 }
